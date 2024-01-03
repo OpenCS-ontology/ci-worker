ee83cf8997016e522e195bb3fa68e57084846d81
Kacper Grzymkowski <kacper.grzymkowski00@gmail.com>
2024-01-03 18:40:30 +0100

    Ignore logs from Jena when combining the files

--- validate_jena.sh
+++ validate_jena.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 mkdir package
-/opt/jena/bin/turtle opencs/ontology/core/*/*.ttl > package/core.ttl
+/opt/jena/bin/turtle opencs/ontology/core/*/*.ttl > package/core.ttl 2> package/riot.log
 /opt/jena/bin/shacl validate --shapes opencs/ontology/shacl_constraints.ttl --data package/core.ttl > package/validation_report.ttl
 /opt/jena/bin/sparql --query /app/validation_short_report.rq --data package/validation_report.ttl
 
